0213a02810942505dfdc76d3d45027e62af19a91
blueprint515 <30677005+blueprint515@users.noreply.github.com>
2017-08-14 11:08:13 +0800

    Add files via upload
    习题21-40

---
+++ ex28.py
@@ -0,0 +1,1 @@
+3 != 4 and not ("testing" != "test" or "Python" == "Python")
---
+++ ex34.py
@@ -0,0 +1,14 @@
+animals = ['bear', 'python', 'peacock', 'tiger', 'lion', 'kangaroo']
+bear = animals[0]
+python = animals[1]
+peacock = animals[2]
+tiger = animals[3]
+lion = animals [4]
+kangaroo = animals [5]
+
+print (animals[0])
+print (animals[1])
+print (animals[2])
+print (animals[3])
+print (animals[4])
+print (animals[5])
